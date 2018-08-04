a46ce184e5992b448d50a1f377652054f648372d
deenski <jakevendegna@gmail.com>
2018-08-04 10:28:25 -0600

    made localssl.sh more verbose, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LocalhostSSL
 ## a zero config ssl cert for your npm http-server
+
+## Dependencies: npm
--- localssl.sh
+++ localssl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo npm install -g http-server
+
 # create localhost ssl cert and key files
 openssl req -x509 -out localhost.crt -keyout localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
@@ -15,3 +17,12 @@ chmod u+x https-server.sh
 
 # create symbolic link
 sudo ln -sf $THISDIR/https-server.sh /bin/ws
+
+echo
+echo "npm http-server installed"
+echo "certificate and key files created"
+echo "https-server.sh created and made executable"
+echo "symbolic link created to https-server.sh"
+echo "just run 'ws' in any directory to start the webserver and serve it via https!"
+echo "see, wasn't that easy?"
+echo "Created by Jake Vendegna - @jakevendegna on twitter @deenski on github"
